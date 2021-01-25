027e6e794e56eff38c0ff94bb59917374218ca1b
Javier Ramirez <apollo.infinity88@gmail.com>
2021-01-25 14:14:45 -0800

    Saving everything before adding Router to this tutorial project

---
+++ src/components/Tasks.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+import Task from './Task'
+
+const Tasks = ({tasks, onDelete, onToggle}) => {
+   
+	return (
+        <>
+            {tasks.map((task) => (
+            <Task key={task.id} onDelete={onDelete} task={task} onToggle={onToggle}/>)
+            )}
+        </>
+    )
+};
+
+export default Tasks;
